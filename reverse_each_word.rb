3e7ea93154a9532b6dba574d61e6ec796a852b87
Lester Young <lyoung070789@gmail.com>
2020-02-12 01:35:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,13 @@
 #Using .each
-def reverse_each_word (string)
-  new_array = string.split(/ /)
-  reverse_word=[]
-  new_array.each {|word| reverse_word << word.reverse}
-  reverse_string=reverse_word.join(" ")
-end
+#def reverse_each_word (string)
+  #new_array = string.split(/ /)
+  #reverse_word=[]
+  #new_array.each {|word| reverse_word << word.reverse}
+  #reverse_string=reverse_word.join(" ")
+#end
 
 #using .collect 
+def reverse_each_word (string)
+  new_array = string.split(/ /)
+  new_array.collect {|word| word.reverse}
+end
